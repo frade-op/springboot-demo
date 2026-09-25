faf8c75ad17e26995caf58541ae3fb9c9123ef39
Felipe Frade de Oliveira Pereira <39383884+frade-op@users.noreply.github.com>
2026-09-25 15:35:09 -0300

    feat: organizing files

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
 
 @RestController
 public class UserController {
